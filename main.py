1eb171f279abcf9ed2420bf60b78b3a23bdbdaa4
Getúlio Werle <getuliowerle10@gmail.com>
2023-09-09 09:37:28 -0400

    adicionado novas páginas

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/usuario/<nome_usuario>;<nome_profissao>')
+@app.route('/usuario', defaults={'nome_usuario':'usuario?', 'nome_profissao':''})   #Valores padrão para os argumentos.
+def usuario(nome_usuario, nome_profissao):
+    dados_usuario = {'profissao':nome_profissao, 'disciplina':'Desenvolvimento para Web III'}
+    return render_template('usuario.html', nome_usuario=nome_usuario, dados_usuario=dados_usuario)
 
 if __name__ == '__main__':
     app.run()
